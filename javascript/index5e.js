87f1087879f14d2a0649f5616d36d545ff49a05b
Abdullah Cheema <abdullahcheema4867@gmail.com>
2026-03-19 01:34:39 +0500

    initial commit

---
+++ javascript/index5e.js
@@ -0,0 +1,38 @@
+//generating random name
+let rand=Math.random()
+let first,second,third
+
+if(rand<0.33){
+    first="Crazy"
+}
+else if(rand<0.66 && rand >=0.33)
+{
+    first="Amazing"
+}
+else{
+    first="Fire"
+}
+
+if(rand<0.33)
+{
+    second ="Engine"
+}
+else if(rand<0.66 & rand >-0.33)
+{
+    second ="Foods"
+}
+else{
+    second="Garments"
+}
+if(rand<0.33){
+    third="Bros"
+}
+else if(rand<0.66 && rand>=0.33)
+{
+    third="limited"
+}
+else
+{
+    third="cars"
+}
+console.log(`${first} ${second} ${third}`)
